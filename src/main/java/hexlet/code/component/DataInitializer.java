f42fc7e9dcacf23d14a67aa848ddcfa8c846e8bb
Daniil <bombyascher@gmail.com>
2024-06-13 21:16:30 +0300

    Fix initialize admin

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -2,21 +2,27 @@ package hexlet.code.component;
 
 import hexlet.code.dto.user.UserCreateDTO;
 import hexlet.code.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DataInitializer implements ApplicationRunner {
     @Autowired
     private UserService userService;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        var admin = new UserCreateDTO();
-        admin.setEmail("hexlet@example.com");
-        admin.setPassword("qwerty");
-        userService.create(admin);
+    public void run(ApplicationArguments args) {
+        try {
+            var admin = new UserCreateDTO();
+            admin.setEmail("hexlet@example.com");
+            admin.setPassword("qwerty");
+            userService.create(admin);
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+        }
     }
 }
