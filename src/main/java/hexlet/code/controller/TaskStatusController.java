196c75f0bda95513b028318d428a7af160f6c6ad
Daniil <bombyascher@gmail.com>
2024-06-14 21:34:16 +0300

    Add X-Total-Count to get response on /api/task_statuses

--- src/main/java/hexlet/code/controller/TaskStatusController.java
+++ src/main/java/hexlet/code/controller/TaskStatusController.java
@@ -9,6 +9,7 @@ import hexlet.code.service.TaskStatusService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +29,11 @@ public class TaskStatusController {
     private TaskStatusService taskStatusService;
 
     @GetMapping
-    List<TaskStatusDTO> index() {
-        return taskStatusService.getAllTaskStatuses();
+    ResponseEntity<List<TaskStatusDTO>> index() {
+        var taskStatuses = taskStatusService.getAllTaskStatuses();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(taskStatuses.size()))
+                .body(taskStatuses);
     }
 
     @GetMapping("/{id}")
