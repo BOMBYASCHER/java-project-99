94ddac2a331883a32f2e524e8bcc6d4d515bba93
Daniil <bombyascher@gmail.com>
2024-07-06 19:29:05 +0300

    Add 'X-Total-Count' header to response on GET api/tasks

--- src/main/java/hexlet/code/controller/TaskController.java
+++ src/main/java/hexlet/code/controller/TaskController.java
@@ -9,6 +9,7 @@ import hexlet.code.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +29,11 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    List<TaskDTO> index() {
-        return taskService.getAllTasks();
+    ResponseEntity<List<TaskDTO>> index() {
+        var tasks = taskService.getAllTasks();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(tasks.size()))
+                .body(tasks);
     }
 
     @GetMapping("/{id}")
