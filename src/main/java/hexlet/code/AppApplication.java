895a17ea5c2f812f4da5a2a880bc33df586327fa
Daniil <bombyascher@gmail.com>
2024-07-12 16:27:29 +0300

    Attempt to fix

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableSentry(dsn = "https://d2b3f7c497d3e9ec9eddd658c17b540a@o4507588692803584.ingest.de.sentry.io/4507588699422800")
+@EnableSentry
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
