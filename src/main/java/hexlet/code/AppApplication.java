0ca9b1bb36762c339b47b5b53f100322bd35c310
Daniil <bombyascher@gmail.com>
2024-07-12 17:22:29 +0300

    Attempt to fix

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,13 +1,11 @@
 package hexlet.code;
 
-import io.sentry.spring.jakarta.EnableSentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableSentry
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
